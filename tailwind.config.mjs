fabaa1871743bfa0b76090e35529605b1a45068f
LuisEspitaleta <Lumiesal@gmail.com>
2024-05-06 22:42:40 -0500

    creando header

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,6 +6,9 @@ export default {
 			'custom-gray': '#171717',
 			'custom-green': '#4DC493',
 		},
+		fontFamily: {
+			lufga: ['Lufga', 'sans-serif'], // Registrar la fuente personalizada 'Lufga'
+		  },
 		extend: {
 			spacing: {
 				'8xl': '1300px',
