8f62177617081bdcdd71b7dc4bba87c3084b2baa
LuisEspitaleta <Lumiesal@gmail.com>
2024-05-09 23:21:55 -0500

    update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		colors: {
-			'custom-gray': '#171717',
-			'custom-green': '#4DC493',
-		},
-		fontFamily: {
-			lufga: ['Lufga', 'sans-serif'], // Registrar la fuente personalizada 'Lufga'
-		  },
+	theme: {				
 		extend: {
+			fontFamily: {
+				lufga: ['Lufga', 'sans-serif'], // Registrar la fuente personalizada 'Lufga'
+				urbanist: ['Urbanist', 'sans-serif'], // Registrar la fuente personalizada 'Lufga'
+			},
+			colors: {
+				'custom-gray': '#171717',
+				'custom-green': '#4DC493',
+				'custom-green-50': '#00c5931a',
+			},
 			spacing: {
 				'8xl': '1300px',
 				'9xl': '1440px',
