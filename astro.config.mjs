d8bd26608382838af914e8513fc81d07f5202728
LuisEspitaleta <Lumiesal@gmail.com>
2024-05-11 02:29:58 -0500

    add icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,17 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  stylesheets: [
+    'node_modules/slick-carousel/slick/slick.css',
+    'node_modules/slick-carousel/slick/slick-theme.css',
+  ],
+
+  // Importa JavaScript de Slick Slider
+  scripts: [
+    {
+      type: 'module',
+      src: 'node_modules/slick-carousel/slick/slick.min.js',
+    },
+  ],
 });
